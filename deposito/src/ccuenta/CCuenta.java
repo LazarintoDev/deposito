f30ac5de372e4e75a44abbc0db2a565fa008bec8
LazarintoDev <alumno.354114@ies-azarquiel.es>
2025-03-04 20:58:22 +0100

    3. Se crea metodo operativa_cuenta que agrupa los metodos del main

--- deposito/src/ccuenta/CCuenta.java
+++ deposito/src/ccuenta/CCuenta.java
@@ -1,6 +1,4 @@
-
 package ccuenta;
-
 
 public class CCuenta {
 
@@ -9,35 +7,55 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    public void operativa_cuenta(String nom, String cue, double sal, double tipo) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta(nom, cue, sal, tipo);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es" + saldoActual);
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+
     }
 
-    public double estado()
-    {
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+    }
+
+    public double estado() {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         saldo = saldo + cantidad;
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         saldo = saldo - cantidad;
     }
 }
